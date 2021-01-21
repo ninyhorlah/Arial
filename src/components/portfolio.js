969cdfc6674a1d462d07816669ababf76df6f0ff
ninyhorlah <58943705+ninyhorlah@users.noreply.github.com>
2021-01-21 15:31:52 +0100

    portfolio section created

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Portfolio = () => {
     return (
         <div>
-            <div>
+            <div className="portfolioInner">
                 <div className="aboutHeader">
                     <img src="/assets/img/headline_heart.svg" alt=""/>
                     <div className="aboutText  storyText">
@@ -14,6 +14,64 @@ const Portfolio = () => {
                     </div>
                 </div>
                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed turpis massa, scelerisque vel diam non.</p>
+
+                 <div className="portfolioTitle">
+                     <div>
+                        <p>ALL</p>
+                     </div>
+                     <div>
+                        <p>WEDDING</p>
+                     </div>
+                     <div>
+                        <p>PARTY</p>
+                     </div>
+                     <div>
+                        <p>VIDEOS</p>
+                     </div>
+                 </div>
+
+                 <div className="portfolioPictures">
+                     <div
+                     data-aos="zoom-in"
+                     >
+                         <img src="/assets/img/portfolio_1.jpg" alt=""/>
+                     </div>
+                     <div
+                     data-aos="zoom-in"
+                     >
+                         <img src="/assets/img/blog_2.jpg" alt=""/>
+                     </div>
+                     <div
+                     data-aos="zoom-in"
+                     >
+                         <img src="/assets/img/blog_3.jpg" alt=""/>
+                     </div>
+                     <div
+                     data-aos="zoom-in"
+                     >
+                         <img src="/assets/img/portfolio_4.jpg" alt=""/>
+                     </div>
+                     <div
+                     data-aos="zoom-in"
+                     >
+                         <img src="/assets/img/blog_bg.jpg" alt=""/>
+                     </div>
+                     <div
+                     data-aos="zoom-in"
+                     >
+                         <img src="/assets/img/portfolio_6.jpg" alt=""/>
+                     </div>
+                     <div
+                     data-aos="zoom-in"
+                     >
+                         <img src="/assets/img/portfolio_7.jpg" alt=""/>
+                     </div>
+                     <div
+                     data-aos="zoom-in"
+                     >
+                         <img src="/assets/img/blog_1.jpg" alt=""/>
+                     </div>
+                 </div>
             </div>
         </div>
     )
